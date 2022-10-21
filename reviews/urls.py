ec8190cb42541bc2ac6f1105a9ca4a485a5b3bef
chaehyun_oh <ohchaehh@gmail.com>
2022-10-21 10:35:17 +0900

    comments/delete;

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,4 +5,9 @@ app_name = "reviews"
 
 urlpatterns = [
     path("<int:review_pk>/comments/", views.comment_create, name="comment_create"),
+    path(
+        "<int:review_pk>/comments/<int:comment_pk>/delete/",
+        views.delete_create,
+        name="delete_create",
+    ),
 ]
