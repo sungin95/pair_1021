d14a29dd83afc4ac0854d8e5aaf37b54749500ae
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2022-10-21 12:48:57 +0900

    add chage_password & detail

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
+    path("password/", views.change_password, name="change_password"),
+    path("<int:pk>/", views.detail, name="detail"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from .forms import CustomUserCreationForm
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+
 
 # Create your views here.
 def index(request):
@@ -48,3 +49,25 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return redirect("accounts:login")
+
+
+def change_password(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("accounts:index")
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/change_password.html", context)
+
+
+def detail(request, pk):
+    user = get_user_model().objects.get(pk=pk)
+    context = {
+        "user": user,
+    }
+    return render(request, "accounts/detail.html", context)
