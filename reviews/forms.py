32459777b774f49a1ca4973bf541fdc70083a43d
tmsnvl95 <tmsnvl95@naver.com>
2022-10-21 11:33:27 +0900

    form_html

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,5 +1,11 @@
 from django import forms
-from .models import Comment
+from .models import Comment, Review
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['title', 'content', 'movie_name', 'grade', 'image']
 
 
 class CommentForm(forms.ModelForm):
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Review, Comment
-from .forms import CommentForm
+from .forms import CommentForm, ReviewForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
@@ -8,13 +8,31 @@ from django.contrib import messages
 # Create your views here.
 
 def index(request):
-    pass
+    reviews = Review.objects.order_by('-pk')
+    context = {
+        'reviews':reviews
+    }
+    return render(request, 'reviews/index.html', context)
+
 
 def detail(request, pk):
     pass
 
 def create(request):
-    pass
+    if request.method == 'POST':
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            review = review_form.save(commit=False) 
+            review.user = request.user 
+            review.save()
+            messages.success(request, '글 작성이 완료되었습니다.')
+            return redirect('reviews:index')
+    else:
+        review_form = ReviewForm()
+    context = {
+        'review_form':review_form
+    }
+    return render(request, 'reviews/form.html', context)
 
 def update(request, pk):
     pass
